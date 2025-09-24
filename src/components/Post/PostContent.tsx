c7c27de8c9d85d6ce7d0aa15638ed4f478a40066
chyeon97 <pcyeon07@gmail.com>
2025-09-24 10:47:27 +0900

    style: 게시글 페이지 반응형 디자인 적용

--- src/components/Post/PostContent.tsx
+++ src/components/Post/PostContent.tsx
@@ -11,6 +11,7 @@ const MarkdownRenderer = styled.div`
   width: 768px;
   margin: 0 auto;
   padding: 100px; 0;
+  word-break: break-all;
   
   line-height: 1.8;
   font-size: 16px;
@@ -87,6 +88,33 @@ const MarkdownRenderer = styled.div`
   code[class*='language-'],
   pre[class*='language-'] {
     tab-size: 2,
+  }
+  
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 80px 20px;
+    line-height: 1.6;
+    font-size: 14px;
+    
+    h1 {
+      font-size: 23px;
+    }
+    
+    h2 {
+      font-size: 20px;
+    }
+    
+    h3 {
+      font-size: 17px;
+    }
+    
+    img {
+      width: 100%;
+    }
+    
+    hr {
+      margin: 50px 0;
+    }
   }
   
 `
--- src/components/Post/PostHead.tsx
+++ src/components/Post/PostHead.tsx
@@ -17,6 +17,10 @@ const PostHeaderWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const BackgroundImage = styled((props: IGatsbyImgProps) => {
@@ -27,6 +31,10 @@ const BackgroundImage = styled((props: IGatsbyImgProps) => {
   height: 400px;
   object-fit: cover;
   filter: brightness(0.25);
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const PostHead: FunctionComponent<PostHeadProps> = function ({
